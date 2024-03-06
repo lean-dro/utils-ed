6d97a7866fe00042534b9d6bf96d9cd8ab686658
lean-dro <leandro.saraiva@sptech.school>
2024-03-06 11:31:10 -0300

    Funções: show, set e remove

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArrayList<T>{
     private Object array[];
 
@@ -18,6 +20,45 @@ public class ArrayList<T>{
     public Integer size(){
         return this.array.length;
     }
+
+    public void remove(int indice){
+        validaIndiceInformado(indice);
+        Object arrayNovo[] = new Object[array.length-1];
+        int contadorArrayNovo = 0;
+        for (int i = 0; i < array.length; i++) {
+
+            if(i!=indice) {
+                arrayNovo[contadorArrayNovo] = this.array[i];
+                contadorArrayNovo++;
+            }
+
+        }
+        this.array = arrayNovo;
+    }
+    public void set(int indice, T valor){
+        validaIndiceInformado(indice);
+
+        this.array[indice] = valor;
+    }
+
+    public void show(){
+        Arrays.stream(this.array).forEach(v->{
+            System.out.print(v);
+            if(v==this.array[size()-1]) System.out.print("");
+            else System.out.print(", ");
+        });
+    }
+
+    private void validaIndiceInformado(int indice){
+        if(indice < 0 || indice >= this.array.length){
+            throw  new ArrayIndexOutOfBoundsException("Indice informado é maior ou menor que a quantidade de posições no vetor");
+        }
+
+
+    }
+
+
+
 
 
 }
